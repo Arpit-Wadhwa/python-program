9b17b9814e70cbe2a370bd98a3bd806038c98313
nemooxx69 <120503298+nemooxx69@users.noreply.github.com>
2024-09-06 11:42:34 +0530

    Add files via upload

---
+++ TRY EXCEPT.py
@@ -0,0 +1,15 @@
+def add(num1, num2):
+     return num1+ num
+def subtract (num1, num2):
+      return num1 - num2
+def multiply ( num1, num2)
+    return num1*num2
+
+def divide (num1 , num2)
+    return num1/num2
+print("select the operation - \n")
+
+   "1. add\n"
+   "2.  subtract\n"
+   "3. multiply \n"
+   "4.  divide \n"
